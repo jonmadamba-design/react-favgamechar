852045c3549e2083b65c22ba4e557a1c5a1a97c5
jonmadamba <jonmadambanw+git@gmail.com>
2020-07-02 16:20:54 -0700

    jonm - favorite game char lookup

---
+++ precache-manifest.a7024483154b366795a8e891f41437b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062e4e1eed7324bb54a35eee264925e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5553dde14c4a1f52208c",
+    "url": "/static/css/main.c5a5f774.chunk.css"
+  },
+  {
+    "revision": "ad4ec9970123e7931d13",
+    "url": "/static/js/2.e6cb5b28.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e6cb5b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5553dde14c4a1f52208c",
+    "url": "/static/js/main.49652c4c.chunk.js"
+  },
+  {
+    "revision": "9a88df53cf648a5f66fd",
+    "url": "/static/js/runtime-main.2d2c88fa.js"
+  }
+]);
